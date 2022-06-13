e9b10c2a1ea86e279b0af71fbc5d17d36abfb15a
Fabian Morales Astorga <fmorales.astorga.15@gmail.com>
2022-06-12 22:37:25 -0400

    cambiando de ciclo for a funciones rec

--- introJs/funciones.js
+++ introJs/funciones.js
@@ -1,19 +1,27 @@
 // Funcion recursiva que, a) muestre en consola los numeros del 1 al 100 de 1 en 1 b) muestre en consola los numeros del 1000 al 500 de 2 en 2
-function mostrarNumeros() {
-	for (i = 0; i <= 100; i++) {
-		console.log(i);
+const mostrarIncremento = (numero) => {
+	console.log(numero);
+	if (numero < 100) {
+		numero++;
+		mostrarIncremento(numero);
 	}
-	for (n = 1000; n >= 500; n -= 2) {
-		console.log(n);
+};
+
+mostrarIncremento(0);
+
+const mostrarDecremento = (limSup) => {
+	console.log(limSup);
+	if (limSup > 500) {
+		limSup -= 2;
+		mostrarDecremento(limSup);
 	}
-}
+};
 
-mostrarNumeros();
-
-// Funciones necesarias para obtener el promedio de notas
-// de un alumno considerando que la suma de notas debe ser
-// el retorno de una funcion y el promedio el retorno de otra funcion
-// notas:6,8,9,2,5,10
+mostrarDecremento(1000);
+// // Funciones necesarias para obtener el promedio de notas
+// // de un alumno considerando que la suma de notas debe ser
+// // el retorno de una funcion y el promedio el retorno de otra funcion
+// // notas:6,8,9,2,5,10
 
 const notas = [6, 8, 9, 2, 5, 10];
 
@@ -25,6 +33,4 @@ function promedio(notas) {
 	let prom = suma / notas.length;
 	return prom;
 }
-
 console.log("El promedio es de ", promedio(notas));
-console.log(notas);
